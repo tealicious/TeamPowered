03ac4c597427368a981d15433dbf7947bf9c9392
Dan <Dan@DESKTOP-HNOEPHQ>
2016-10-27 14:35:59 -0400

    Split Name into First and Last Name

--- TeamPowered/TeamPowered/person.cs
+++ TeamPowered/TeamPowered/person.cs
@@ -8,7 +8,8 @@ namespace TeamPowered
 {
     class person
     {
-           public string Name { get; set; }
-           public string Address { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
     }
 }
